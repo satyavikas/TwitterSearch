e30e4ef2dc0bd7c9d5c0f96616f4b7be158f0065
SManda <smanda@us.imshealth.com>
2018-04-25 13:59:51 -0400

    Testing Ingestion speed without Sentimental Analysis

--- src/extractors/TwitterBear_BMX.py
+++ src/extractors/TwitterBear_BMX.py
@@ -11,25 +11,53 @@ import traceback
 import twitter
 from src.tweet_transformer import transform_tweet
 from src.solr_indexer import index_to_solr
+import logging
 
-# BMX
-access_token = '636984272-kV5vKBpz9QJ46UZP8yVmXSulLqLYh55suNinHzmM'
-access_token_secret = 'HJWYLX0NE3bdZc2Llyo0CBpc9bHT03mgyiMoSAn70'
-consumer_key = 'QSzL0ZJstcIUtEvVv1UgyA'
-consumer_secret = 'ljGWKSgxY807frB5uInEmJ506OzI1MZ4eOeoxKZw7g'
 
-api = twitter.Api(consumer_key,
-                  consumer_secret,
-                  access_token,
-                  access_token_secret)
+def extractor(worker_id):
 
-results = api.GetStreamFilter(track=["Donald Trump", "India","F-1", "H1B", "H1-B", "Immigration"])
+    # BMX
+    access_token = '636984272-kV5vKBpz9QJ46UZP8yVmXSulLqLYh55suNinHzmM'
+    access_token_secret = 'HJWYLX0NE3bdZc2Llyo0CBpc9bHT03mgyiMoSAn70'
+    consumer_key = 'QSzL0ZJstcIUtEvVv1UgyA'
+    consumer_secret = 'ljGWKSgxY807frB5uInEmJ506OzI1MZ4eOeoxKZw7g'
 
-for tweet in results:
-    try:
-        transformed_tweet = transform_tweet(tweet)
-        index_to_solr(transformed_tweet)
-    except Exception as e:
-        print(e)
-        traceback.print_exc
-        sys.exit()
+    # Logging Settings
+    logger = logging.getLogger(__name__)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.FileHandler('/var/log/TwitterSearch/TwitterBear_BMX_worker_{0}.log'.format(worker_id))
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    api = twitter.Api(consumer_key, consumer_secret, access_token, access_token_secret)
+
+    results = api.GetStreamFilter(track=["Donald Trump", "India","F-1", "H1B", "H1-B", "Immigration"])
+
+    count = 0
+    error_count = 0
+    for tweet in results:
+        count += 1
+        try:
+            transformed_tweet = transform_tweet(tweet)
+            try:
+                index_to_solr(transformed_tweet)
+            except Exception as e:
+                print(e)
+                logger.error("Exeption: {0} | Error Count so far: {2}   |   Tweet: {1}".format(e, json.dumps(tweet), error_count))
+                pass
+        except Exception as e:
+            error_count += 1
+            print(e)
+            traceback.print_exc
+            logger.error("Exeption: {0} | Error Count so far: {2}   |   Tweet: {1}".format(e, json.dumps(tweet), error_count))
+            pass
+        if count % 1000 == 0:
+            logger.info("Processed {0} so far. Error Count so far: {1}".format(count, error_count))
+
+
+if __name__ == '__main__':
+    # The worker_id for this Script.
+    worker_id = str(sys.argv[1])
+    extractor(worker_id)
